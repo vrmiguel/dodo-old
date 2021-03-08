5c90afccecf6f0b2ee6f7b7a57a6e14a46123215
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-03-08 18:15:56 -0300

    Add REPL command hinting

--- src/editor.rs
+++ src/editor.rs
@@ -1,26 +1,102 @@
-use std::path::PathBuf;
+use std::{collections::HashSet, path::PathBuf};
 
 use colored::Colorize;
-/// A simple wrapper over a Rustyline editor
+
 use rustyline::{self, error::ReadlineError};
+use rustyline::hint::{Hint, Hinter};
+use rustyline::Context;
+use rustyline_derive::{Completer, Helper, Highlighter, Validator};
 
 static HISTORY_FILE_NAME: &'static str = "ron_history";
 
+
+impl Hint for CommandHint {
+    fn display(&self) -> &str {
+        &self.display
+    }
+
+    fn completion(&self) -> Option<&str> {
+        if self.complete_up_to > 0 {
+            Some(&self.display)
+        } else {
+            None
+        }
+    }
+}
+
+fn editor_hints() -> HashSet<CommandHint> {
+    let mut set = HashSet::new();
+    set.insert(CommandHint::new("add",     "a"));
+    set.insert(CommandHint::new("group",    "g"));
+    // set.insert(CommandHint::new("remove task",  "remove t"));
+    set.insert(CommandHint::new("remove",  "r"));
+    set.insert(CommandHint::new("help", "h"));
+    set
+}
+
+#[derive(Hash, Debug, PartialEq, Eq)]
+struct CommandHint {
+    display: String,
+    complete_up_to: usize,
+}
+
+impl Hinter for EditorHinter {
+    type Hint = CommandHint;
+
+    fn hint(&self, line: &str, pos: usize, _ctx: &Context<'_>) -> Option<CommandHint> {
+        if pos < line.len() {
+            return None;
+        }
+
+        self.hints
+            .iter()
+            .filter_map(|hint| {
+                if pos > 0 && hint.display.starts_with(&line[..pos]) {
+                    Some(hint.suffix(pos))
+                } else {
+                    None
+                }
+            })
+            .next()
+    }
+}
+
+#[derive(Completer, Helper, Validator, Highlighter)]
+struct EditorHinter {
+    hints: HashSet<CommandHint>,
+}
+
+
+impl CommandHint {
+    fn new(text: &str, complete_up_to: &str) -> CommandHint {
+        assert!(text.starts_with(complete_up_to));
+        CommandHint {
+            display: text.into(),
+            complete_up_to: complete_up_to.len(),
+        }
+    }
+
+    fn suffix(&self, strip_chars: usize) -> CommandHint {
+        CommandHint {
+            display: self.display[strip_chars..].to_owned(),
+            complete_up_to: self.complete_up_to.saturating_sub(strip_chars),
+        }
+    }
+}
+
+/// A simple wrapper over a Rustyline editor
 pub struct Editor {
-    inner: rustyline::Editor<()>,
+    inner: rustyline::Editor<EditorHinter>,
 }
 
 impl Editor {
     /// Returns a new rustyline::Editor with history loaded in (if it exists)
     pub fn new(config_path: &PathBuf) -> Self {
-        let mut inner = rustyline::Editor::<()>::new();
-        // let hinter = EditorHinter {
-        //     hints: match mode {
-        //         ManagerMode::Tutorial   => tutorial_hints(),
-        //         ManagerMode::Playground => playground_hints()
-        //     }
-        // };
-        // editor.set_helper(Some(hinter));
+        let mut inner = rustyline::Editor::<EditorHinter>::new();
+        let hinter = EditorHinter {
+            hints: editor_hints()
+        };
+        inner.set_helper(Some(hinter));
         if inner
             .load_history(&config_path.join(HISTORY_FILE_NAME))
             .is_err()
